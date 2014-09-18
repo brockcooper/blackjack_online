67bd334fcc4dde2cc0754daa18108c74643c5e76
brockcooper <brockcooper@gmail.com>
2014-09-18 12:55:15 -0600

    First commit

---
+++ main.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+
+set :sessions, true
+  
+get '/' do
+  erb :set_name
+end
+
+post '/set_name' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
+get '/game' do 
+  session[:deck] = []
+    ["Hearts", "Diamonds", "Clubs", "Spades"].each do |suit|
+      ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"].each do |value|
+         session[:deck] << [suit, value]
+      end
+    end
+  session[:player_cards] = []
+  session[:player_cards] << session[:deck].pop
+  erb :game
+end
+
+get '/hello' do 
+  erb :"/user/hello"
+end
+
+
+
+
