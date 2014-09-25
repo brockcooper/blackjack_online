57b15d39c07102fe98348207f6f756b3ccbd70f9
brockcooper <brockcooper@gmail.com>
2014-09-25 08:44:41 -0600

    Debugging

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 #require 'sinatra/reloader'
 require 'pry'
 
-set :sessions, true
+#set :sessions, true
 use Rack::Session::Cookie, :key => 'rack.session',
 :path => '/',
 :secret => 'your_secret'
